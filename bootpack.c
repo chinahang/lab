f5d333a4aeb30f3261df8aa7fa568ce283ae7f43
chinahang <chenhang19900930@outlook.com>
2021-12-04 15:24:03 +0800

    多任务代码敲完

--- bootpack.c
+++ bootpack.c
@@ -13,7 +13,8 @@ struct TSS32 {
 	int es, cs, ss, ds, fs, gs;
 	int ldtr, iomap;
 };
-void task_b_main(void);
+
+void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void)
 {
@@ -21,7 +22,7 @@ void HariMain(void)
 	struct FIFO32 fifo;
 	char s[40];
 	int fifobuf[128];
-	struct TIMER *timer, *timer2, *timer3 ,*timer_ts;
+	struct TIMER *timer, *timer2, *timer3;
 	int mx, my, i, cursor_x, cursor_c, task_b_esp;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
@@ -38,41 +39,38 @@ void HariMain(void)
 		'2', '3', '0', '.'
 	};
 	struct TSS32 tss_a, tss_b;
-	struct SEGMENT_DESCRIPTOR* gdt = (struct SEGMENT_DESCRIPTOR*)ADR_GDT;
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+
 	init_gdtidt();
 	init_pic();
 	io_sti(); /* IDT/PIC的初始化已经完成，于是开放CPU的中断 */
 	fifo32_init(&fifo, 128, fifobuf);
-	enable_mouse(&fifo, 512, &mdec);
-	init_keyboard(&fifo, 256);
 	init_pit();/*定时器的中断初始化*/
+    init_keyboard(&fifo, 256);
+  	enable_mouse(&fifo, 512, &mdec);
+
 	io_out8(PIC0_IMR, 0xf8); /* 开放PIC1和键盘中断(11111001) */
 	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
 
-	memtotal = memtest(0x00400000, 0xfffffff);
-	memman_init(memman);
-	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
-	memman_free(memman, 0x00400000, memtotal - 0x00400000);
+
 
 	timer = timer_alloc();
 	timer_init(timer, &fifo, 10);
 	timer_settime(timer, 1000);
-
 	timer2 = timer_alloc();
 	timer_init(timer2, &fifo, 3);
 	timer_settime(timer2, 300);
-
 	timer3 = timer_alloc();
 	timer_init(timer3, &fifo, 1);
 	timer_settime(timer3, 50);
 
-	timer_ts = timer_alloc();
-	timer_init(timer_ts, &fifo, 2);
-	timer_settime(timer_ts, 2);
-
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 	init_palette();  //初始化调色板
 	shtctl = shtctl_init(memman, binfo->vram, binfo->scrnx, binfo->scrny);
-	sht_back = sheet_alloc(shtctl);
+	sht_back  = sheet_alloc(shtctl);
 	sht_mouse = sheet_alloc(shtctl);
 	sht_win = sheet_alloc(shtctl);
 	buf_back = (unsigned char*)memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
@@ -104,11 +102,11 @@ void HariMain(void)
 	tss_a.iomap = 0x40000000;
 	tss_b.ldtr = 0;
 	tss_b.iomap = 0x40000000;
-	set_segmdesc(gdt + 3, 103, (int)&tss_a, AR_TSS32);
-	set_segmdesc(gdt + 4, 103, (int)&tss_b, AR_TSS32);
+	set_segmdesc(gdt + 3, 103, (int) &tss_a, AR_TSS32);
+	set_segmdesc(gdt + 4, 103, (int) &tss_b, AR_TSS32);
 	load_tr(3 * 8);
-	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
-	tss_b.eip = (int)&task_b_main;
+	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
+	tss_b.eip = (int) &task_b_main;
 	tss_b.eflags = 0x00000202; /* IF = 1; */
 	tss_b.eax = 0;
 	tss_b.ecx = 0;
@@ -124,7 +122,8 @@ void HariMain(void)
 	tss_b.ds = 1 * 8;
 	tss_b.fs = 1 * 8;
 	tss_b.gs = 1 * 8;
-	*((int*)0x0fec) = (int)sht_back; /*随便找的内存地址*/
+	*((int *) (task_b_esp + 4)) = (int) sht_back;
+	mt_init();
 
 	for (;;) {
 		io_cli();
@@ -133,14 +132,10 @@ void HariMain(void)
 		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
-			if (i == 2) {
-				farjmp(0, 4 * 8);
-				timer_settime(timer_ts, 2);
-			}
-			else if (256 <= i && i <= 511) { /*键盘数据*/
+			if (256 <= i && i <= 511) { /*键盘数据*/
 				sprintf(s, "%02X", i - 256);
 				putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-				if (i < 256 + 0x54) {
+				if (i < 0x54 + 256) {
 					if (keytable[i - 256] != 0 && cursor_x < 144) {/*一般字符*/
 						s[0] = keytable[i - 256];
 						s[1] = 0;
@@ -152,7 +147,7 @@ void HariMain(void)
 				putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF," ",  1);
 				cursor_x -= 8;
 				}
-				boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 29, cursor_x + 7, 43);
+				boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
 				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
 			}
 			else if (512 <=i && i<=767) { /*鼠标数据*/
@@ -290,35 +285,39 @@ void make_textbox8(struct SHEET* sht, int x0, int y0, int sx, int sy, int c) {
 	boxfill8(sht->buf, sht->bxsize, c,           x0 - 1, y0 - 1, x1 + 0, y1 + 0);
 	return;
 }
-void task_b_main(void)
+
+void task_b_main(struct SHEET *sht_back)
 {
 	struct FIFO32 fifo;
-	struct TIMER* timer_ts;
-	int i, fifobuf[128], count = 0;
-	char s[11];
-	struct SHEET* sht_back;
-	sht_back = (struct SHEET*)*((int*)0xfec);
+	struct TIMER *timer_put, *timer_1s;
+	int i, fifobuf[128], count = 0, count0 = 0;
+	char s[12];
 
 	fifo32_init(&fifo, 128, fifobuf);
-	timer_ts = timer_alloc();
-	timer_init(timer_ts, &fifo, 1);
-	timer_settime(timer_ts, 500);
+	timer_put = timer_alloc();
+	timer_init(timer_put, &fifo, 1);
+	timer_settime(timer_put, 1);
+	timer_1s = timer_alloc();
+	timer_init(timer_1s, &fifo, 100);
+	timer_settime(timer_1s, 100);
 
 	for (;;) {
 		count++;
-		sprintf(s, "%010d", count);
-		putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
 			io_sti();
-			io_hlt();
-		}
-		else {
+		} else {
 			i = fifo32_get(&fifo);
 			io_sti();
 			if (i == 1) { /* 超时间5sec */
-				farjmp(0, 3 * 8); /* 返回任务 */
-				timer_settime(timer_ts, 2);
+				sprintf(s, "%11d", count);
+				putfonts8_asc_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 11);
+				timer_settime(timer_put, 1);
+			} else if (i == 100) {
+				sprintf(s, "%11d", count - count0);
+				putfonts8_asc_sht(sht_back, 0, 128, COL8_FFFFFF, COL8_008484, s, 11);
+				count0 = count;
+				timer_settime(timer_1s, 100);
 			}
 		}
 	}
